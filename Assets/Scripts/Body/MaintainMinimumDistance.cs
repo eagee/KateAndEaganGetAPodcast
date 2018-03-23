5ac6b639d706caf567381f3a13e5367ad9c57602
eagee <eagix@sbcglobal.net>
2018-03-22 22:59:50 -0400

    Added code so that elbows will mainatain a distance from the spine so that the arms won't intersect with the character body.

---
+++ Assets/Scripts/Body/MaintainMinimumDistance.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MaintainMinimumDistance : MonoBehaviour {
+    public Transform TargetObject;
+    public float XDistance = 0f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 targetPos = TargetObject.transform.position;
+        Vector3 myPos = this.transform.position;
+        
+        // If we're to the right of the target object
+        if (targetPos.x < myPos.x) 
+        {
+            float distance = myPos.x - targetPos.x;
+            if(distance < XDistance)
+            {
+                myPos.x += XDistance - distance;
+                this.transform.position = myPos;
+            }
+        }
+        else if (targetPos.x > myPos.x)
+        {
+            float distance = targetPos.x - myPos.x;
+            if (distance < XDistance)
+            {
+                myPos.x -= XDistance - distance;
+                this.transform.position = myPos;
+            }
+        }
+    }
+}
