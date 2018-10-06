0ac8067b615403c6082b7d5de44f809c53aac286
eagee <eagix@sbcglobal.net>
2018-10-06 14:08:37 -0400

    Updated to Unity 2018.2.11f1

--- Assets/Scripts/TimedEvents/TimedEventManager.cs
+++ Assets/Scripts/TimedEvents/TimedEventManager.cs
@@ -2,19 +2,20 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 // Timeline events keep a timer running throughout the scene and send messages to objects
 [System.Serializable]
 public class TimedEventManager : MonoBehaviour {
-    public List<TimedEventEntry> EvenEntries;
+    public List<TimedEventEntry> EventEntries;
     public List<GameObject> Observers;
     private float m_EventTimer;
-
+    
     void Update()
     {                            
         m_EventTimer += Time.deltaTime;
         GetComponent<TextMesh>().text = m_EventTimer.ToString();
 
-        foreach(var entry in EvenEntries)
+        foreach(var entry in EventEntries)
         {
             if(m_EventTimer > entry.TimeOffset && entry.Triggered == false)
             {
